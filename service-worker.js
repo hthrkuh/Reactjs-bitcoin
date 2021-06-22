6b430c55913cb10488e3691f5d1fbdd011af7a87
unknown <hthrkuh8@gmail.com>
2021-06-22 18:48:59 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Reactjs-bitcoin/precache-manifest.c17e97328f4d75fd2cb1c713513bb05a.js"
+  "/Reactjs-bitcoin/precache-manifest.73c1e9741fdb804357614805f6094801.js"
 );
 
 self.addEventListener('message', (event) => {
